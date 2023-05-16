676fe9f22ee71e006621cebafb1c494488c21b7a
etiry <7997357+etiry@users.noreply.github.com>
2023-05-16 19:30:10 -0400

    Add JS to display books

--- script.js
+++ script.js
@@ -15,3 +15,28 @@ function addBookToLibrary(title, author, pages, read) {
 addBookToLibrary('Happy Place', 'Emily Henry', 385, false);
 addBookToLibrary('The House of Eve', 'Sadeqa Johnson', 369, false);
 addBookToLibrary('Einstein\'s Dreams', 'Alan Lightman', 144, true);
+
+const container = document.querySelector('.container')
+
+for (book of myLibrary) {
+  const newBook = document.createElement('div');
+  newBook.classList.add('card');
+
+  const title = document.createElement('p');
+  title.textContent = book.title;
+  newBook.appendChild(title);
+
+  const author = document.createElement('p');
+  author.textContent = book.author;
+  newBook.appendChild(author);
+
+  const pages = document.createElement('p');
+  pages.textContent = `${book.pages} pages`;
+  newBook.appendChild(pages);
+
+  const read = document.createElement('p');
+  book.read ? read.textContent = 'Read' : read.textContent = 'Not read';
+  newBook.appendChild(read);
+
+  container.appendChild(newBook);
+}
