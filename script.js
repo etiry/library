e4cc328ef1af7d7de59f04b8c90337951de5ab59
etiry <7997357+etiry@users.noreply.github.com>
2023-05-18 17:29:04 -0400

    Fix add book form functionality

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 let myLibrary = [];
 
+addBookToLibrary('Happy Place', 'Emily Henry', 385, false);
+addBookToLibrary('The House of Eve', 'Sadeqa Johnson', 369, false);
+addBookToLibrary('Einstein\'s Dreams', 'Alan Lightman', 144, true);
+addBookToLibrary('The Likeness', 'Tana French', 466, true);
+displayBooks();
+
 function Book(title, author, pages, read) {
 	this.title = title;
 	this.author = author;
@@ -20,35 +26,56 @@ function closeAddBookForm() {
   document.querySelector('.add-book-form').style.display = 'none';
 }
 
-addBookToLibrary('Happy Place', 'Emily Henry', 385, false);
-addBookToLibrary('The House of Eve', 'Sadeqa Johnson', 369, false);
-addBookToLibrary('Einstein\'s Dreams', 'Alan Lightman', 144, true);
-addBookToLibrary('The Likeness', 'Tana French', 466, true);
+function submitAddBookForm(e) {
+  e.preventDefault();
 
-const container = document.querySelector('.container')
+  title = document.querySelector('input[id=title]');
+  author = document.querySelector('input[id=author]');
+  pages = document.querySelector('input[id=pages]');
+  read = document.querySelector('input[id=read]');
 
-for (book of myLibrary) {
-  const newBook = document.createElement('div');
-  newBook.classList.add('card');
+  addBookToLibrary(title.value, author.value, pages.value, read.checked);
 
-  const title = document.createElement('p');
-  title.textContent = book.title;
-  newBook.appendChild(title);
+  title.value = '';
+  author.value = '';
+  pages.value = '';
+  read.checked = false;
 
-  const author = document.createElement('p');
-  author.textContent = book.author;
-  newBook.appendChild(author);
+  displayBooks();
 
-  const pages = document.createElement('p');
-  pages.textContent = `${book.pages} pages`;
-  newBook.appendChild(pages);
-
-  const read = document.createElement('p');
-  book.read ? read.textContent = 'Read' : read.textContent = 'Not read';
-  newBook.appendChild(read);
-
-  container.appendChild(newBook);
+  closeAddBookForm();
 }
 
-const openButton = document.querySelector('.openButton')
-openButton.addEventListener('click', openAddBookForm)
+function displayBooks() {
+  const bookContainer = document.querySelector('.book-container')
+  bookContainer.textContent = '';
+
+  for (book of myLibrary) {
+    const newBook = document.createElement('div');
+    newBook.classList.add('card');
+
+    const title = document.createElement('p');
+    title.textContent = book.title;
+    newBook.appendChild(title);
+
+    const author = document.createElement('p');
+    author.textContent = book.author;
+    newBook.appendChild(author);
+
+    const pages = document.createElement('p');
+    pages.textContent = `${book.pages} pages`;
+    newBook.appendChild(pages);
+
+    const read = document.createElement('p');
+    book.read ? read.textContent = 'Read' : read.textContent = 'Not read';
+    newBook.appendChild(read);
+
+    bookContainer.appendChild(newBook);
+  }
+}
+
+const openButton = document.querySelector('.open-button');
+openButton.addEventListener('click', openAddBookForm);
+
+const submitButton = document.querySelector('.submit-button');
+submitButton.addEventListener('click', submitAddBookForm);
