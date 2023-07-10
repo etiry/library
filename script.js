4965d1bcdb589b9264472fdaf1fa97ed651d770c
etiry <7997357+etiry@users.noreply.github.com>
2023-07-10 11:57:08 -0400

    Refactor using class instead of constructor

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
 let myLibrary = [];
 
+class Book {
+  constructor(title, author, pages, read) {
+   	this.title = title;
+	  this.author = author;
+	  this.pages = pages;
+	  this.read = read; 
+  }
+}
+
 addBookToLibrary('Happy Place', 'Emily Henry', 385, false);
 addBookToLibrary('The House of Eve', 'Sadeqa Johnson', 369, false);
 addBookToLibrary('Einstein\'s Dreams', 'Alan Lightman', 144, true);
 addBookToLibrary('The Likeness', 'Tana French', 466, true);
 displayBooks();
-
-function Book(title, author, pages, read) {
-	this.title = title;
-	this.author = author;
-	this.pages = pages;
-	this.read = read;
-}
 
 function addBookToLibrary(title, author, pages, read) {
 	newBook = new Book(title, author, pages, read);
